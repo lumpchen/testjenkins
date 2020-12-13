e1921d4f05748f1c9f91297848f063255504e6a7
Mike Li <lumpchen@gmail.com>
2020-12-13 16:54:10 +0800

    Add testcase, should get success result

---
+++ src/test/java/jenkinstest/Test.java
@@ -0,0 +1,12 @@
+package jenkinstest;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+public class Test extends TestCase {
+	
+    
+    public void testHello() {
+    	Assert.assertEquals("hello", "hello");
+    }
+}
